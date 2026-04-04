4ea2b26b8edca4899149a0be1caa7e620e750df8
guanyuluo-hub <guanyu.luo@gmail.com>
2026-04-04 13:15:56 +0200

    Delete src/main/java/com/example/workflowapproval/model directory

--- src/main/java/com/example/workflowapproval/model/User.java
+++
@@ -1,9 +0,0 @@
-package com.example.workflowapproval.model;
-
-public class User {
-    private String id;
-    private String username;
-    private String email;
-
-    // Constructors, Getters, and Setters
-}
